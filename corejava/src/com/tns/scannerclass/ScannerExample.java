bafb229e608135bea5b088c258bb2eaee3714a02
aalia <aalia@192.168.1.2>
2024-06-23 16:09:21 +0530

    commit message

---
+++ corejava/src/com/tns/scannerclass/ScannerExample.java
@@ -0,0 +1,23 @@
+package com.tns.scannerclass;
+
+import java.util.Scanner;
+
+//demo of scanner class
+
+public class ScannerExample {
+
+	public static void main(String[] args) {
+          Scanner sc=new Scanner(System.in);
+          
+          System.out.println("enter you are name");
+          String name=sc.nextLine();
+       
+          
+          System.out.println("enter the student id");
+          int si=sc.nextInt();
+          
+          System.out.println("enter the percentage");
+   float per=sc.nextFloat();
+	}
+
+}
